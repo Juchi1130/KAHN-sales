2cce16578ac1ee7bc95c5eccef69f823a850aa2a
Juergen <juergen.kahn@me.com>
2016-05-24 22:48:39 +0200

    BankAccount (Accountnumber, Amount, CreditLimit, FirstName, OpeningDate)

--- src/domain/BankAccountDBI.java
+++ src/domain/BankAccountDBI.java
@@ -1,9 +1,70 @@
 package domain;
 
-public class BankAccountDBI {
+import java.time.LocalDateTime;
 
-	public BankAccountDBI() {
-		// TODO Auto-generated constructor stub
+public class BankAccountDBI {
+	private Long	   		id;
+	private LocalDateTime 	openingDate;
+	private String    		firstname;
+	private Double    		amount;
+	private Double    		creditlimit;
+	
+	
+	public BankAccountDBI(Long id, LocalDateTime openingDate, String firstname, Double amount, Double creditlimit) {
+		setId( id );
+		setOpeningDate(openingDate);
+		setFirstname (firstname);
+		setAmount(amount);
+		setCreditlimit (creditlimit);
+	}
+	
+	public BankAccountDBI( String firstname, Double amount ) {
+		this( null, null, firstname, amount, null );
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId( Long id ) {
+		this.id = id;
+	}
+	public LocalDateTime getOpeningDate() {
+	
+		return openingDate;
 	}
 
+	public void setOpeningDate(LocalDateTime openingDate) {
+		this.openingDate = openingDate;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public Double getCreditlimit() {
+		return creditlimit;
+	}
+	
+	public void setCreditlimit(Double creditlimit) {
+		this.creditlimit = creditlimit;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", openingDate=" + openingDate + ", firstname=" + firstname + ", amount=" + amount
+				+ ", creditlimit=" + creditlimit + "]\n";
+	}
 }
